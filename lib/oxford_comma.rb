6861a1d8ee5d370aab5ada38261500cf31b77e8e
Robert Watson <spartan83@gmail.com>
2017-12-10 05:09:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,9 @@ if array.size == 1
 elsif array.size ==2
   two = array[0]+" and "+array[1]
   two
+elsif array.size ==3
+three = array[0] + array[1] + " and " + array[2]
+
 else
   array[array.size-1] =  "and #{array.last}"
   array
